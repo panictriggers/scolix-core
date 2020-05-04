ccbb9d10ea397fbaa290d9f06f7236bf3948ab48
Hugo Woesthuis <hugow2011@outlook.com>
2020-05-04 15:07:54 +0200

    custom printlib add (basic)

--- scolix-core-daemon/src/main.rs
+++ scolix-core-daemon/src/main.rs
@@ -1,10 +1,11 @@
 mod build_cfg;
-
+extern crate scolix_core_cli;
 fn printintro(){
-    println!("{}", format!("Scolix v{}; Build:{} on {}", build_cfg::get_buildinfo().version, build_cfg::get_buildinfo().buildc, build_cfg::get_buildinfo().arch));
+    println!("{}", format!("Scolix v{}; Build:{} on {}\n\n", build_cfg::get_buildinfo().version, build_cfg::get_buildinfo().buildc, build_cfg::get_buildinfo().arch));
 }
 
 // Entry point
 fn main() {
     printintro();
+    scolix_core_cli::printinfo("fuck");
 }
