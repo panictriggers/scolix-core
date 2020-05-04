dcf2a0d6748f20a4ec9431e01dc2ebcd861627b7
Hugo Woesthuis <hugow2011@outlook.com>
2020-05-04 19:04:18 +0200

    got servers to work

--- scolix-core-daemon/src/build_cfg.rs
+++ scolix-core-daemon/src/build_cfg.rs
@@ -5,7 +5,7 @@ pub struct Build{
 }
 
 pub fn get_buildinfo() -> Build {
-	let buildinf = Build{version: String::from("0.1.0"), buildc: String::from("0000"),arch: String::from("win64")} ;
+	let buildinf = Build{version: String::from("0.1.0"), buildc: String::from("development-20200504-1d8e55"),arch: String::from("win64")} ;
 	return buildinf;
 
 }
