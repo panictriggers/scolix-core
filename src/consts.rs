acc4b6fc32f95e81aba219b61522e2833b3c283e
Hugo Woesthuis <hugow2011@outlook.com>
2020-10-21 10:10:00 +0200

    redo
    Een nieuw begin!

---
+++ src/consts.rs
@@ -0,0 +1,1 @@
+pub const BUILDVERSION: &'static str = "0.1.0-dev-2faab8e";
---
+++ src/main.rs
@@ -0,0 +1,17 @@
+//
+// Copyright (C) 2020 PanicTriggers.
+// All rights reserved.
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+//
+
+// Don't build with warnings or unsafe code
+#![deny(warnings)]
+#![deny(unsafe_code)]
+
+
+mod consts;
+
+fn main() {
+    println!("Scolix Core {}", consts::BUILDVERSION);
+}
